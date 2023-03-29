f9cd5c9c16d2b030c9d6db1b99febc1d9e2e610b
Darius Garcia <dariusgarcia888@gmail.com>
2023-03-28 19:09:20 -0700

    formatted files

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,12 @@
-import { withClerkMiddleware, getAuth } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server";
-import type { NextRequest } from 'next/server'
-  
-  export default withClerkMiddleware((request: NextRequest) => {   
-    return NextResponse.next()
-  })
+import { withClerkMiddleware, getAuth } from '@clerk/nextjs/server';
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+export default withClerkMiddleware((request: NextRequest) => {
+  return NextResponse.next();
+});
 
 // Stop Middleware running on static files
-export const config = { matcher:  '/((?!_next/image|_next/static|favicon.ico).*)'};
+export const config = {
+  matcher: '/((?!_next/image|_next/static|favicon.ico).*)',
+};
--- pages/post/index.tsx
+++ pages/post/index.tsx
@@ -1,9 +1,13 @@
-import Header from '@/features/post/header';
 import React from 'react';
+import Head from 'next/head';
+import Header from '@/features/post/header';
 
 export default function index() {
   return (
     <div>
+      <Head>
+        <title>Blog App - Creators</title>
+      </Head>
       <Header />
     </div>
   );
